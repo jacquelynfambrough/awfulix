70b3f1883a6a10813a45791549aedf5bf55b3271
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-26 21:23:19 -0700

    added routes for user and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   #user routes
-
+  get '/users/new', to: 'users#new', as: 'new_user'
+  #sessions routes
+  get '/login', to: 'sessions#new', as: 'new_session'
   #movie routes
 
   #review routes
