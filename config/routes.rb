943e59e9db40772919164363bf597cca3dd5e71c
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-26 21:38:19 -0700

    added more user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   root to: 'users#index'
   #user routes
   get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users/create', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update' 
+  delete '/users/:id', to: 'users#destroy'
+
   #sessions routes
   get '/login', to: 'sessions#new', as: 'new_session'
+
   #movie routes
 
   #review routes
