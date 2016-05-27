6d008389f076e0828821a3c4d4b4934e2fa5abc8
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-26 21:39:20 -0700

    added login methods (sessions new and create)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,25 @@
+
 class SessionsController < ApplicationController
+
   def new
     @user = User.new
     render :new
   end
 
   def create
-    user_params = params.require(:user).permit(:email, :password, :username)
-    @user = user.confirm(user_params)
+    user_params = params.require(:user).permit(:email, :password)
+    @user = User.confirm(user_params)
     if @user
       login(@user)
-      redirect_to new_user_path
+      redirect_to @user
+    else
+      redirect_to session_path
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 end
