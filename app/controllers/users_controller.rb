de0ee51036d7d65e0ded087a707ee39e9ef2f072
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-26 22:05:11 -0700

    login functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def create
+    @user = User.create(user_params)
+    @user.save
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
 end
