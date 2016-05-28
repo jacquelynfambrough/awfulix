516439e4e86cf3f7713efb87e58055941a50ba51
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-28 12:46:00 -0700

    destroy/delete now working for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,12 @@ class UsersController < ApplicationController
     render :show
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   #user routes
-  get '/', to: 'users#index', as: 'home'
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  delete '/users/:id', to: 'users#destroy', as: 'delete_user'
 
   #sessions routes
   get '/login', to: 'sessions#new', as: 'login'
