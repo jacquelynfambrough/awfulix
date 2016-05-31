10a0358b537d9d877f4f404a509adc794aafa2b0
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-31 10:21:13 -0700

    bug fix. json data was not being captured properly..

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,23 +7,25 @@ class MoviesController < ApplicationController
     if params[:search]
     response = HTTParty.get('https://www.omdbapi.com/?s=' + params[:search] + '&r=json')
     pars = response.parsed_response
+    search = pars["Search"]
       @movies = []
-      pars.Search.each do |movie|
+      search.each do |movie|
       @movie = Movie.new
-      @movie.title = movie.Title
-      @movie.poster = movie.Poster
-      @movie.year = movie.Year
+      @movie.title = movie["Title"]
+      @movie.poster = movie["Poster"]
+      @movie.year = movie["Year"]
       @movies << @movie
     end
   else
     response = HTTParty.get('https://www.omdbapi.com/?s=' + 'cool' + '&r=json')
     pars = response.parsed_response
+    search = pars["Search"]
       @movies = []
-      pars.Search.each do |movie|
+      search.each do |movie|
       @movie = Movie.new
-      @movie.title = movie.Title
-      @movie.poster = movie.Poster
-      @movie.year = movie.Year
+      @movie.title = movie["Title"]
+      @movie.poster = movie["Poster"]
+      @movie.year = movie["Year"]
       @movies << @movie
       end
     end
