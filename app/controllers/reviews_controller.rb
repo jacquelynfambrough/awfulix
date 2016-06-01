b55172f7943f497380322cbf5a88094af70dbd9d
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-06-01 16:35:55 -0700

    new, create, and index implemented. only rendering content so far

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,42 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @user = User.find(params[:id])
+    @movie = Movie.find(params[:id])
+    @reviews = @movie.reviews
+  end
+
+  def new
+    @movie = Movie.find(params[:id])
+    @review = Review.new
+    render :new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @user = current_user
+    @movie = Movie.find(params[:id])
+    @movie.reviews << @review
+    @user.reviews << @review
+    @movie.save
+    @user.save
+    @review.save
+
+    redirect_to movie_reviews_path(@movie)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating, :subject)
+  end
 end
