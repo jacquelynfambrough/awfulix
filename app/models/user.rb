2ac1883354529480fab77e2be81ca229eaa267ea
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-06-02 03:45:45 -0700

    raty rate tool successfully added and styled.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
   has_many :movies, through: :reviews
-
+  ratyrate_rater
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
