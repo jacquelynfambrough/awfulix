72d965ec46a02fb8cebfe6e2d164f6a985220bf1
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2018-05-07 23:39:28 -0700

    cleared git cache

--- app/models/movie.rb
+++
@@ -1,9 +0,0 @@
-class Movie < ActiveRecord::Base
-  has_many :reviews
-  has_many :users, through: :reviews
-  has_many :ratings
-
-  def average_rating
-    ratings.sum(:score) / ratings.size
-  end
-end
--- db/migrate/20160531223408_add_imdb_id_to_movies.rb
+++
@@ -1,5 +0,0 @@
-class AddImdbIdToMovies < ActiveRecord::Migration
-  def change
-    add_column :movies, :imdbid, :string
-  end
-end
