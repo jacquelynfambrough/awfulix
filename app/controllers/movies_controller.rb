5e93d05e268591f55682a047ad908e8ee6ca8e82
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2018-05-19 15:55:21 -0700

    user can only rate movie if logged in

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,8 @@ class MoviesController < ApplicationController
 
   def index
       if params[:search]
-        response = HTTParty.get('https://www.omdbapi.com/?s=' + params[:search] + '&r=json')
+        # search params only pull up these attributes: title, year, imdbid, type & poster
+        response = HTTParty.get('https://www.omdbapi.com/?apikey=' + Rails.application.credentials[:ombd_api_key] + '&s=' + params[:search] + '&r=json')
         # if response is success, insert movies pulled up in search into empty array
         if response.success?
           search = response["Search"]
@@ -14,8 +15,7 @@ class MoviesController < ApplicationController
               new_movie.title = movie["Title"]
               new_movie.poster = movie["Poster"]
               new_movie.year = movie["Year"]
-              new_movie.rated = movie["Rated"]
-              new_movie.plot = movie["Plot"]
+
             end
             @movies << mov
           end
@@ -29,24 +29,33 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:movie_id])
-    movie_response = HTTParty.get('https://www.omdbapi.com/?t=' + @movie.title + '&y=&plot=full&r=json')
-    movie_response_short_plot = HTTParty.get('https://www.omdbapi.com/?t=' + @movie.title + '&y=&r=json')
+    movie_response = HTTParty.get('https://www.omdbapi.com/?apikey=' + Rails.application.credentials[:ombd_api_key] + '&t=' + @movie.title + '&y=&plot=full&r=json')
+    movie_response_short_plot = HTTParty.get('https://www.omdbapi.com/?apikey=' + Rails.application.credentials[:ombd_api_key] + '&t=' + @movie.title + '&y=&r=json')
     @plotShort = movie_response_short_plot["Plot"]
     @plotFull = movie_response
     @genre = movie_response["Genre"]
     @maturityRating = movie_response["Rated"]
-    @rottenRating = movie_response["Plot"]
-    @imdbRating = movie_response["Plot"]
+    # @rottenRating = movie_response["Plot"]
+    # @imdbRating = movie_response["Plot"]
     @reviews = @movie.reviews
     @reviews.each do |review|
     @user = User.find_by_id(review.user_id)
   end
-    if @rating
-      @rating = Rating.where(movie_id: @movie.id, user_id: current_user.id).first
+
+    if @rating == true # if there is a rating, display the rating, otherwise, set rating score to 0
+      if @user == true  # if the user is logged in, tie in user id to allow contribution to rating score, if not, simply show the rating
+        @rating = Rating.where(movie_id: @movie.id, user_id: current_user.id).first
+      else
+        @rating = Rating.where(movie_id: @movie.id).first
+      end #end secondary if case for existence of user
     else
-      @rating = Rating.create(movie_id: @movie.id, user_id: current_user.id, score: 0)
+      if @user == true
+        @rating = Rating.create(movie_id: @movie.id, user_id: current_user.id, score: 0)
+      else
+        @rating = Rating.create(movie_id: @movie.id, score: 0)
+      end #end secondary if case for existence of user
     end
     render :show
-  end
+  end #end to main if case for review score
 
 end
